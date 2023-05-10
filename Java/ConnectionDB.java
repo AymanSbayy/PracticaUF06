bd8b207e6b6b9112e61904d575f7fa865b4f2ace
atarensi <99744155+atarensi@users.noreply.github.com>
2023-05-10 09:56:28 +0200

    Create ConnectionDB.java
    Connection

---
+++ Java/ConnectionDB.java
@@ -0,0 +1,27 @@
+package ConnectionDB;
+
+import java.sql.*;
+
+public class ConnectionDB {
+    public static Connection con;
+
+    public static Connection openCon(String ip, String bd, String usuari, String password){
+        con = null;
+
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://"+ ip +"/"+ bd + usuari + password);
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+
+        return con;
+    }
+
+    public static void closeCon(){
+        try{
+            if (con != null) con.close();
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
