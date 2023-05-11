db60983a85b6338c1bd7351c37693bb97fa7e074
atarensi <99744155+atarensi@users.noreply.github.com>
2023-05-11 10:45:58 +0200

    Update candidaturesDAO.java
    Perfeccionant el DAO

--- Java/Candidatures/candidaturesDAO.java
+++ Java/Candidatures/candidaturesDAO.java
@@ -1,17 +1,18 @@
-package Candidatures;
-import Connection.ConnectionDB;
-import Interf.CRUD;
+package Objectes.Candidatures;
+import ConnectionDB.ConnectionDB;
+import CRUD.CRUD;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-public class candidaturesDAO extends ConnectionDB implements CRUD<candidatures> {
+public class CandidaturaDAO extends ConnectionDB implements CRUD<Candidatura> {
+
     @Override
-    public boolean create(candidatures candidatures) {
-        String query = " INSERT INTO candidatures (eleccio_id,codi_candidatura,nom_curt,nom_llarg,codi_acumulacio_provincia,codi_acumulacio_ca,codi_acumulario_nacional)"
-                + " values (?, ?, ?, ?, ?)";
+    public boolean create(Candidatura candidatures) {
+        String query = " INSERT INTO candidatures (eleccio_id,codi_candidatura,nom_curt,nom_llarg,codi_acumulacio_provincia,codi_acumulacio_ca,codi_acumulacio_nacional)"
+                + " values (?, ?, ?, ?, ?, ?, ?)";
         try{
             PreparedStatement preparedStmt = con.prepareStatement(query);
 
@@ -21,7 +22,7 @@ public class candidaturesDAO extends ConnectionDB implements CRUD<candidatures> 
             preparedStmt.setString(4, candidatures.getNom_llarg());
             preparedStmt.setString(5, candidatures.getCodi_acumulacio_provincia());
             preparedStmt.setString(6, candidatures.getCodi_acumulacio_ca());
-            preparedStmt.setString(7, candidatures.getCodi_acumulario_nacional());
+            preparedStmt.setString(7, candidatures.getcodi_acumulacio_nacional());
             preparedStmt.execute();
             return true;
         }catch (Exception e){
@@ -31,25 +32,28 @@ public class candidaturesDAO extends ConnectionDB implements CRUD<candidatures> 
     }
 
     @Override
-    public candidatures read(int id) {
-        String query = "SELECT * FROM candidatures WHERE id = ?";
-        candidatures c = null;
+    public List<Candidatura> read(int id) {
+        String query = "SELECT * FROM candidatures WHERE candidatura_id = ?";
+        Candidatura c = new Candidatura();
+        List<Candidatura> camps = new ArrayList<>();
         try {
             PreparedStatement preparedStmt = con.prepareStatement(query);
 
             preparedStmt.setInt(1, id);
 
             ResultSet rs = preparedStmt.executeQuery();
-            if (rs.next()) {
+            while (rs.next()) {
+                c.setCandidatura_id(rs.getInt("candidatura_id"));
                 c.setEleccio_id(rs.getInt("eleccio_id"));
                 c.setCodi_candidatura(rs.getString("codi_candidatura"));
                 c.setNom_curt(rs.getString("nom_curt"));
                 c.setNom_llarg(rs.getString("nom_llarg"));
                 c.setCodi_acumulacio_provincia(rs.getString("codi_acumulacio_provincia"));
                 c.setCodi_acumulacio_ca(rs.getString("codi_acumulacio_ca"));
-                c.setCodi_acumulario_nacional(rs.getString("codi_acumulario_nacional"));
+                c.setcodi_acumulacio_nacional(rs.getString("codi_acumulacio_nacional"));
+                camps.add(c);
             }
-            return c;
+            return camps;
         } catch (Exception e) {
             System.out.println(e);
             return null;
@@ -57,20 +61,19 @@ public class candidaturesDAO extends ConnectionDB implements CRUD<candidatures> 
     }
 
     @Override
-    public boolean update(candidatures candidatures, int id) {
-        String query = "UPDATE candidatures SET eleccio_id = ?, codi_candidatura = ?, nom_curt = ?," +
-                "nom_llarg = ?, codi_acumulacio_provincia = ?, codi_acumulacio_ca = ?, codi_acumulario_nacional = ? WHERE id = ?";
+    public boolean update(Candidatura candidatures, int id) {
+        String query = "UPDATE candidatures SET codi_candidatura = ?, nom_curt = ?," +
+                "nom_llarg = ?, codi_acumulacio_provincia = ?, codi_acumulacio_ca = ?, codi_acumulacio_nacional = ? WHERE candidatura_id = ?";
         try{
             PreparedStatement preparedStmt = con.prepareStatement(query);
 
-            preparedStmt.setInt(1, candidatures.getEleccio_id());
-            preparedStmt.setString(2, candidatures.getCodi_candidatura());
-            preparedStmt.setString(3, candidatures.getNom_curt());
-            preparedStmt.setString(4, candidatures.getNom_llarg());
-            preparedStmt.setString(5, candidatures.getCodi_acumulacio_provincia());
-            preparedStmt.setString(6, candidatures.getCodi_acumulacio_ca());
-            preparedStmt.setString(7, candidatures.getCodi_acumulario_nacional());
-            preparedStmt.setInt(8, id);
+            preparedStmt.setString(1, candidatures.getCodi_candidatura());
+            preparedStmt.setString(2, candidatures.getNom_curt());
+            preparedStmt.setString(3, candidatures.getNom_llarg());
+            preparedStmt.setString(4, candidatures.getCodi_acumulacio_provincia());
+            preparedStmt.setString(5, candidatures.getCodi_acumulacio_ca());
+            preparedStmt.setString(6, candidatures.getcodi_acumulacio_nacional());
+            preparedStmt.setInt(7, id);
             preparedStmt.execute();
 
             return true;
@@ -82,17 +85,16 @@ public class candidaturesDAO extends ConnectionDB implements CRUD<candidatures> 
 
     @Override
     public boolean delete(int id) {
-        String query = "DELETE FROM candidatures WHERE id = ?";
+        String query = "DELETE FROM candidatures WHERE candidatura_id = ?";
 
         try {
             PreparedStatement preparedStmt = con.prepareStatement(query);
 
             preparedStmt.setInt(1, id);
 
-            ResultSet rs = preparedStmt.executeQuery();
+            preparedStmt.execute();
 
-            if (rs.next()) return true;
-            else return false;
+            return true;
         }catch (Exception e){
             System.out.println(e);
             return false;
@@ -102,7 +104,7 @@ public class candidaturesDAO extends ConnectionDB implements CRUD<candidatures> 
     @Override
     public boolean exists(int id) {
         String query = "SELECT * FROM candidatures WHERE candidatura_id = ?";
-        candidatures c = null;
+        Candidatura c = null;
         try {
             PreparedStatement preparedStmt = con.prepareStatement(query);
 
@@ -136,25 +138,27 @@ public class candidaturesDAO extends ConnectionDB implements CRUD<candidatures> 
     }
 
     @Override
-    public List<candidatures> all() {
+    public List<Candidatura> all() {
         String query = "SELECT * FROM candidatures";
-        List<candidatures> camps = new ArrayList<>();
+        List<Candidatura> camps = new ArrayList<>();
+        Candidatura c = new Candidatura();
         try {
             PreparedStatement preparedStmt = con.prepareStatement(query);
 
             ResultSet rs = preparedStmt.executeQuery();
 
             while (rs.next()){
-                candidatures c = null;
+                c.setCandidatura_id(rs.getInt("candidatura_id"));
                 c.setEleccio_id(rs.getInt("eleccio_id"));
                 c.setCodi_candidatura(rs.getString("codi_candidatura"));
                 c.setNom_curt(rs.getString("nom_curt"));
                 c.setNom_llarg(rs.getString("nom_llarg"));
                 c.setCodi_acumulacio_provincia(rs.getString("codi_acumulacio_provincia"));
                 c.setCodi_acumulacio_ca(rs.getString("codi_acumulacio_ca"));
-                c.setCodi_acumulario_nacional(rs.getString("codi_acumulario_nacional"));
+                c.setcodi_acumulacio_nacional(rs.getString("codi_acumulacio_nacional"));
                 camps.add(c);
             }
+
             return camps;
         }catch (Exception e){
             System.out.println(e);
